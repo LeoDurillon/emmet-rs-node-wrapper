6163fe108dd80e228e1d6e76d3bacb504777749c
LeoDurillon <durillon.leo@outlook.com>
2025-05-24 10:18:04 +0200

    feat: trim and check input

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,44 @@
 #![deny(clippy::all)]
 
-use emmet_rs::{checker::input_correctly_close, statement::Statement};
+use emmet_rs::{checker::{input_correctly_close,input_correctly_structured}, statement::Statement};
 
 #[macro_use]
 extern crate napi_derive;
 
 #[napi]
 fn expand(input:String) -> Option<String>{
-  if !input_correctly_close(&input){
+  let trimmed_input = input.trim().split(' ').collect::<Vec<&str>>();
+  let mut input_start= 0; 
+  for (i,str) in trimmed_input.iter().enumerate() {
+    if i == trimmed_input.len()-1 ||  str.chars().any(|x| x == '+' || x == '>' || x == '<' ) {
+      break;
+    }
+    input_start += 1;
+  }
+  let before_input= trimmed_input[..input_start].join(" ");
+  let input = trimmed_input[input_start..].join(" ");
+  
+  if !input_correctly_close(&input) || !input_correctly_structured(&input){
     return None
   }
-  Some(Statement::new(&input, 0).parse())
+
+  Some(format!("{before_input} {}",Statement::new(&input, 0).parse()))
+}
+
+#[cfg(test)]
+mod test_expand {
+    use crate::expand;
+
+  #[test]
+  fn should_expand(){
+    assert_eq!(Some("<div></div>".to_string()),expand("div".to_string()))
+  }
+
+  #[test]
+  fn should_ignore_before(){
+    assert_eq!(Some("return <div></div>".to_string()),expand("return div".to_string()));
+    assert_eq!(Some("return <div> test</div>".to_string()),expand("return div< test".to_string()));
+  }
+
+
 }
